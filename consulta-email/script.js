058166802699b8f4e1dd2e1f728092dd7ac53b1a
uniconsulta <marcosdalleprane2@gmail.com>
2026-07-02 18:48:25 -0300

    Update script.js

--- consulta-email/script.js
+++ consulta-email/script.js
@@ -1,3 +1,4 @@
+alert("SCRIPT NOVO - TESTE");
 const API_URL = "https://script.google.com/macros/s/AKfycbxjZc4ZAbMdHEf38zuprYlcnzwDQsWiSf64DHX48RPtqGB7ShiHncpGZurRSisZXShbvg/exec";
 
 const logos = {
